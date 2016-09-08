8bec9c3a32ba5a2d2a70a1de06254c42deae87fd
Cristóbal Maturana <cri.maturana@gmail.com>
2016-09-08 20:29:03 -0300

    Clases, Atributos y objetos

---
+++ clase1.php
@@ -0,0 +1,18 @@
+<?php
+
+	class Persona{
+		// Atributos
+		public $nombre = "Pedro";
+
+		// Metodos
+		public function hablar($mensaje){
+			echo $mensaje;
+		}
+	}
+
+	$persona = new Persona();
+	
+	echo $persona->nombre;
+	$persona->hablar("Saludos");
+
+?>
