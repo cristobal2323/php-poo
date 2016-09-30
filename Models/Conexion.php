7175c26ad100016edb3e04dbdb2333042bc89738
Cristóbal Maturana <cri.maturana@gmail.com>
2016-09-29 21:20:49 -0300

    trabajo final

---
+++ Models/Conexion.php
@@ -0,0 +1,25 @@
+<?php namespace Models;
+
+	class Conexion{
+		private $datos = array(
+			"host" => "localhost",
+			"user" => "root",
+			"pass" => "root",
+			"db" => "proyecto"
+		);
+		private $con;
+
+		public function __construct(){
+			$this->con = new \mysqli($this->datos['host'],$this->datos['user'],$this->datos['pass'],$this->datos['db']);
+		}
+
+		public function consultasSimple($sql){
+			$this->con->query($sql);
+		}
+
+		public function consultaRetorno($sql){
+			$datos = $this->con->query($sql);
+			return $datos;
+		}
+	}
+?>
